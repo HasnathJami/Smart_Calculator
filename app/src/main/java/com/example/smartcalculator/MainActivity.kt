2a46256fca012b7320756ceaffce681ee3b1254b
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-04-21 04:25:46 +0600

    Calculator Initial Implementation

--- app/src/main/java/com/example/smartcalculator/MainActivity.kt
+++ app/src/main/java/com/example/smartcalculator/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.example.smartcalculator
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.smartcalculator.ui.theme.MediumGray
 import com.example.smartcalculator.ui.theme.SmartCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,24 +18,37 @@ class MainActivity : ComponentActivity() {
         setContent {
             SmartCalculatorTheme {
                 // A surface container using the 'background' color from the theme
-                testComposable()
+                // testComposable()
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
+
             }
         }
     }
 }
 
-@Composable
-fun testComposable() {
-    CalculatorButton(symbol = "1", modifier = Modifier) {
-        
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SmartCalculatorTheme {
-        testComposable()
-    }
-}
+//@Composable
+//fun testComposable() {
+//    CalculatorButton(symbol = "1", modifier = Modifier) {
+//
+//    }
+//}
+//
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    SmartCalculatorTheme {
+//        testComposable()
+//    }
+//}
